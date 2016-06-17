bd711b5ebffd95612eb594c5657c4c04859f4721
Eziohao <eziohao@gmail.com>
2016-06-17 16:27:17 -0400

    add another http request
    adding another request

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var passport = require('passport');
 var https = require('https');
 
-var postDataNonce = JSON.stringify({
-    'url': "http://www.apple.com",
+var postDataNonce = JSON.stringify({ //set request nonce data
+    'url': "http://localhost:3000",
     "cuid": "S-SCLLOGIN-SCL1_ADMIN",
     "realm": "S-SCLLOGIN-SCL1"
 })
-var nonce;
-var nonceRequest = {
+
+var jwtToken;
+var nonceRequest = { //set request headers
     host: "authn-4-stg.run.covisintrnd.com",
     path: "/authn/nonce",
     port: 443,
@@ -24,12 +25,35 @@ var nonceRequest = {
     }
 
 };
+var jwtRequest = {
+    host: 'apistg.np.covapp.io',
+    path: '/authn/v4/sessionToken/nonce/validate',
+    port: 443,
+    method: "POST",
+    headers: {
+        "ContentType": "application/vnd.com.covisint.platform.authn.nonce.v1+json",
+        "x-requestor": 'S-SCLLOGIN-SCL1_ADMIN',
+        "x-realm": 'S-SCLLOGIN-SCL1',
+        "Accept": 'application/vnd.com.covisint.platform.nonce.response.v1+json',
+        "x-requestor-app": 'facebook_login',
+        "realmId": 'S-SCLLOGIN-SCL1',
+        "Content-Length": Buffer.byteLength(postDatajwt)
+        
+    }
+}
+var postDatajwt = {
+    "realm": "S-SCLLOGIN-SCL1",
+    "nonce": "",
+    "url": "localhost:3000/profile"
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'FaceBook Login' });
 });
 
-router.get('/facebook_login', passport.authenticate('facebook'));
+router.get('/facebook_login', passport.authenticate('facebook')); //login to facebook
 
 router.get('/auth/facebook/callback',
     passport.authenticate('facebook', { failureRedirect: '/' }),
@@ -37,27 +61,44 @@ router.get('/auth/facebook/callback',
         console.log('good');
         res.redirect('/profile');
     });
-router.get('/profile',
+router.get('/profile', //jumping to profile page
     function(req, res) {
-
-
-
-
-        var post_req_nonce = https.request(nonceRequest, function(res) {
+        var post_req_nonce = https.request(nonceRequest, function(res) { //post for getting nonce
             console.log('send request');
             var buffer = "";
             res.setEncoding("utf-8");
             res.on("data", function(data) {
                 buffer += data;
+
             });
             res.on("end", function() {
-                console.log(buffer);
+         
+                buffer = JSON.parse(buffer);
+                postDatajwt.nonce = buffer.nonce;
+                jwtRequest["headers"]["Content-Length"]=Buffer.byteLength(postDatajwt)
+                
+                var post_req_jwt = https.request(jwtRequest, function(res) {
+                    jwtToken = "";
+                    console.log(jwtRequest.headers);
+                    res.setEncoding("utf-8");
+                    res.on("data", function(data) {
+                        jwtToken += data;	
+                    });
+                    res.on("end", function() {
+                        console.log(jwtToken);
+                    })
+                })
+                console.log("send nonce");
+                console.log(postDatajwt);
+                post_req_jwt.write(JSON.stringify(postDatajwt));
+                post_req_jwt.end();
             });
         })
-        
+
         post_req_nonce.write(postDataNonce);
         post_req_nonce.end();
-        res.render('profile', { user: req.user });
+
+        res.render('profile', { user: req.user }); //rendering user profile on the page
 
     })
 
